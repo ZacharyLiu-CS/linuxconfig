c6f5bf7ed4c2df6bc99dbdb24f778f13fc19ba29
zhenliu <liuzhenm@mail.ustc.edu.cn>
2019-12-03 14:39:32 +0800

    update the config

--- manjaro_zhfont.sh
+++
@@ -1,3 +0,0 @@
-sudo pacman -S ttf-roboto noto-fonts ttf-dejavu
-sudo pacman -S wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei
-sudo pacman -S noto-fonts-cjk adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts
