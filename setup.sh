98fe18f51972a089d1ad91b8636fb31aa0d913a3
liuzhen <liuzhenm@mail.ustc.edu.cn>
2020-03-22 07:53:52 +0800

    add more config

--- setup.sh
+++ setup.sh
@@ -8,3 +8,15 @@ sudo pacman -S nodejs
 sudo yay -S zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s /bin/zsh
+git config --global user.email "liuzhenm@mail.ustc.edu.cn"
+git config --global user.name "liuzhen"
+sudo pacman -S net-tools
+sudo pacman -S gdb
+sudo pacman -S wireshark-qt
+sudo pacman -S openssh
+sudo systemctl enable ssh
+sudo systemctl start sshd
+sudo pacman -S tmux
+cp ~/linuxconfig/.tmux.conf ~
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
