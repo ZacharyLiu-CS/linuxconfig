460d536cb8462c92e87d27d6bc08ad047a4fd038
ZacharyLiu-CS <liuzhenm@mail.ustc.edu.cn>
2020-07-24 16:13:21 +0800

    change the security url to mirrors

--- set_ubuntu.sh
+++ set_ubuntu.sh
@@ -1,4 +1,5 @@
 sudo sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+sudo sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 sudo apt update
 sudo apt upgrade
 sudo apt install -f vim
