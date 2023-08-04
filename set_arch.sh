6a5abefcdc1978fed800a1e723970a7f696359c9
ZacharyLiu-CS <liuzhenm@mail.ustc.edu.cn>
2023-08-04 11:47:33 +0800

    add tmux plugin manager and tmux save & restore plugin

--- set_arch.sh
+++ set_arch.sh
@@ -15,7 +15,14 @@ sudo pacman -S gdb
 sudo pacman -S openssh
 sudo systemctl enable ssh
 sudo systemctl start sshd
+# install the tmux and tpm plugin
 sudo pacman -S tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ~/linuxconfig/.tmux.conf ~
+
+
 git clone git@github.com:zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+sudo pacman -S ripgrep # use to search from file
+sudo pacman -S bear # use to generate from makefile to compile_commandls.json
--- set_ubuntu.sh
+++ set_ubuntu.sh
@@ -9,7 +9,7 @@ sudo apt install -f git
 sudo apt install -f htop
 sudo apt install -f nodejs
 sudo apt install -f curl
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+bash ./install_oh_my_zsh.sh
 chsh -s /bin/zsh
 sudo cp ~/linuxconfig/.vimrc ~
 git config --global user.email "liuzhenm@mail.ustc.edu.cn"
@@ -19,12 +19,15 @@ git clone https://github.com/ZacharLiu-CS/linuxconfig.git ~
 sudo apt install openssh-server
 sudo systemctl enable ssh
 sudo systemctl start sshd
+# install the tmux and tpm plugin
 sudo apt install -f tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ~/linuxconfig/.tmux.conf ~
-git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-git clone git://github.com/joelthelion/autojump.git ~
+git clone git@github.com:zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone git@github.com:joelthelion/autojump.git ~
 cd ~/autojump && ./install.py
 sudo apt install ripgrep # use to search from file
-sudo apt install brear # use to generate from makefile to compile_commandls.json
+sudo apt install bear # use to generate from makefile to compile_commandls.json
